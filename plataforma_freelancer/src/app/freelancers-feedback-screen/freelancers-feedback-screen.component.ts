08197738be1980b25740a666bcdea9c8170f5d40
Morgana Galamba <mbfg2@cin.ufpe.br>
2022-05-10 11:57:13 -0300

    Adiciona componente da tela de feedback do freelancer

---
+++ plataforma_freelancer/src/app/freelancers-feedback-screen/freelancers-feedback-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-freelancers-feedback-screen',
+  templateUrl: './freelancers-feedback-screen.component.html',
+  styleUrls: ['./freelancers-feedback-screen.component.css']
+})
+export class FreelancersFeedbackScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
