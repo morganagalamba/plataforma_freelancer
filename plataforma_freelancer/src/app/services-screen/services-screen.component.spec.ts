52721da3a5b0e4403ba52bed8b4fb963840965eb
misabelf <maria.fernandes@invillia.com>
2022-05-11 14:05:02 -0300

    corrige 2 erros dos testes

--- plataforma_freelancer/src/app/services-screen/services-screen.component.spec.ts
+++ plataforma_freelancer/src/app/services-screen/services-screen.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ServicesScreenComponent } from './services-screen.component';
 
@@ -8,7 +9,10 @@ describe('ServicesScreenComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ServicesScreenComponent ]
+      declarations: [ ServicesScreenComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+      ]
     })
     .compileComponents();
   });
